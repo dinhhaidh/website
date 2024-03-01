645767cdca7f11e96c4c6ccac5c510fad8c6885a
dinhhaidh <phamhaiqt02@gmail.com>
2024-03-01 12:18:53 +0700

    first commit

---
+++ .history/js/main_20240229223019.js
@@ -0,0 +1,29 @@
+/*=============== SHOW MENU ===============*/
+$(document).ready(function(){
+    $('#nav-toggle').click(function(){
+        $('#nav-menu').addClass('show-menu')
+    });
+    $('#nav-close').click(function(){
+        $('#nav-menu').removeClass('show-menu')
+    });
+    $('.nav__link').click(function(){
+        $('#nav-menu').removeClass('show-menu')
+    })
+
+
+
+
+    
+    /*=============== CHANGE BACKGROUND HEADER ===============*/
+    $(window).scroll(function() {
+        $('#header').toggleClass('blur-header', $(this).scrollTop() >= 50);
+
+        /*=============== SHOW SCROLL UP ===============*/ 
+        // Khi cuộn cao hơn 350 chiều cao khung nhìn, hãy thêm lớp hiển thị cuộn vào thẻ có lớp cuộn lên
+        $('#scroll-up').toggleClass('show-scroll', $(this).scrollTop() >= 350);
+
+        $('.active-link').addClass('nav_menu')
+        // $()
+    });
+    /*=============== ADD BLUR HEADER ===============*/
+})
